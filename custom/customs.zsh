c52d1480e3a98cd48e412c36f4ca370810f813d0
Debdutto Chakraborty <debdutto.chakraborty@gmail.com>
2023-12-27 13:45:02 +0700

    standaradizing

--- custom/customs.zsh
+++ custom/customs.zsh
@@ -1,11 +1,12 @@
 
 echo "-- Custom Started --"
 
-echo "-- Basics Started --"
-source $ZSH_CUSTOM/basics.sh1
-echo "-- Basics Done --"
+source $ZSH_CUSTOM/basics.jsh
+source $ZSH_CUSTOM/alias.jsh
 
 ## Keep Zshrc in Sync
 cp $ZSH_CUSTOM/.zshrc $HOME/.zshrc
+## Update hosts file
+cat $ZSH_CUSTOM/.hosts /etc/hosts > /dev/null
 
 echo "-- Custom Done --"
